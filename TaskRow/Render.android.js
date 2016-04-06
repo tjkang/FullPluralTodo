e6589bfd0411f315bf94d53d45c5727d1e7f7195
TJ Kang <tjkang81@gmail.com>
2016-04-06 21:24:04 +0900

    Animated the taskrow component for android platform

--- TaskRow/Render.android.js
+++ TaskRow/Render.android.js
@@ -5,24 +5,45 @@ const {
   Text,
   TouchableHighlight,
   Image,
+  Animated,
 } = React;
 
-const localStyle = React.StyleSheet.create({
-  doneButton: {
-    borderRadius: 5,
-    padding: 5,
-  },
-});
-
 export default function render(styles) {
+  const doneAnimation = new Animated.ValueXY();
+
+  const localStyle = React.StyleSheet.create({
+    doneButton: {
+      borderRadius: 5,
+      padding: 5,
+    },
+    row: {
+      transform: doneAnimation.getTranslateTransform(),
+    },
+  });
+
+  function animatedPress() {
+    Animated.spring(doneAnimation, {
+      tension: 2,
+      friction: 3,
+      toValue: {
+        x: -800,
+        y: 0,
+      },
+    }).start();
+
+    setTimeout(() => {
+        this.onDonePressed();
+    }, 1000);
+  }
+
   return (
-    <View style={styles.container}>
+    <Animated.View style={[styles.container, localStyle.row]}>
         <Text
           style={styles.label}
         >{this.props.todo.task}</Text>
 
       <TouchableHighlight
-        onPress={this.onDonePressed.bind(this)}
+        onPress={animatedPress.bind(this)}
         style={localStyle.doneButton}
         underlayColor="#ddd"
       >
@@ -30,6 +51,6 @@ export default function render(styles) {
           source={require('../images/done.png')}
         />
       </TouchableHighlight>
-    </View>
+    </Animated.View>
   );
 }
